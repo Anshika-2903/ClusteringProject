751f4decc3f9c98fd6f9c0868c2e1ab136243fa8
Anshika Prajapati <97735230+Anshika-2903@users.noreply.github.com>
2025-01-16 00:57:57 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,14 +1,13 @@
 # requirements.py
 !pip install pandas numpy matplotlib seaborn hdbscan scikit-learn
 import hdbscan
-from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import numpy as np
-
 import pandas as pd
 from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 from scipy.cluster.hierarchy import dendrogram, linkage
-import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
